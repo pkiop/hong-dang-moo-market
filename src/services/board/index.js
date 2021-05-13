dc93e3a08ac2d8c61cef70594c56944cff74b976
pkiop <pkiop@naver.com>
2021-05-13 23:56:02 +0900

    Feat: Add board services

--- src/services/board/index.js
+++ src/services/board/index.js
@@ -4,8 +4,47 @@ exports.listBoard = async () => {
   try {
     const boards = await BoardModel.find({}).exec();
     return boards;
-  } catch(err) {
+  } catch (err) {
     console.error(err);
     return [];
+  }
+};
+
+exports.getBoardById = async (boardId) => {
+  try {
+    const board = await BoardModel.findOne({ id: boardId }).exec();
+    return board;
+  } catch (err) {
+    console.error(err);
+    return {};
+  }
+};
+
+exports.addBoard = async ({ boardId, title, content }) => {
+  try {
+    await BoardModel.create({ id: boardId, title, content }).exec();
+  } catch (err) {
+    console.error(err);
+    return {};
+  }
+};
+
+exports.updateBoard = async ({ boardId, title, content }) => {
+  try {
+    const query = { id: boardId };
+    await BoardModel.updateOne(query, { title, content }).exec();
+  } catch (err) {
+    console.error(err);
+    return {};
+  }
+};
+
+exports.deleteBoard = async ({ boardId }) => {
+  try {
+    const query = { id: boardId };
+    await BoardModel.deleteOne(query).exec();
+  } catch (err) {
+    console.error(err);
+    return {};
   }
 };
