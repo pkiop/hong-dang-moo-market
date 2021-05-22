c3ce1bccf616a896df7dd87153b413187d2b23d7
pkiop <pkiop@naver.com>
2021-05-22 12:30:39 +0900

    Add update, delete button

--- client/src/components/Write/index.js
+++ client/src/components/Write/index.js
@@ -3,37 +3,64 @@ import './style.scss';
 
 const imageLink =
   'http://itimg.chosun.com/sitedata/image/202001/14/2020011400634_0.jpg';
-function Index({ setData }) {
-  const [title, setTitle] = useState('');
-  const test = {
-    id: 6,
-    title: title,
-    category: 'A',
-    time: 13,
-    money: 100000,
-    user: 'user1',
-    imageLink: imageLink,
+function Write({ setData, setBoardData, boardData, setVisible }) {
+  const [title, setTitle] = useState(setData.title ?? '');
+
+  const updateBoardData = () => {
+    setData((state) => {
+      const id = boardData.id;
+      const newState = state.map((board) => {
+        if (board.id !== id) {
+          return board;
+        } else {
+          return {
+            id: id,
+            title: title,
+          };
+        }
+      });
+      setVisible(false);
+      setBoardData(null);
+      return newState;
+    });
   };
-  return (
-    <div className="write">
-      <span className="fixed">글 제목</span>
-      <div className="item">
-        글 제목 :{' '}
-        <input
-          onChange={(event) => {
-            setTitle(event.target.value);
-          }}
-        />
+  if (boardData === null) {
+    return (
+      <div className="write">
+        <span className="fixed">글 제목</span>
+        <div className="item">
+          글 제목 :{' '}
+          <input
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+          />
+        </div>
+        <button onClick={() => setData((state) => [...state, test])}>
+          Add
+        </button>
       </div>
-      {/* // spread 연산자 */}
-      <button onClick={() => setData((state) => [...state, test])}>
-        Clear
-      </button>
-    </div>
-  );
+    );
+  } else {
+    // 여기는 수정하기
+    return (
+      <div className="write">
+        <span className="fixed">글 제목</span>
+        <div className="item">
+          글 제목 :{' '}
+          <input
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+          />
+        </div>
+        <button onClick={updateBoardData}>Update</button>
+      </div>
+    );
+  }
 }
 
-export default Index;
+export default Write;
 
 // position
 // static, fixed, absolute, relative
