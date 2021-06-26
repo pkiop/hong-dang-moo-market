b8d6623a51288ec958e4a56c6a9e5b26371135cb
pkiop <pkiop@naver.com>
2021-06-26 12:36:48 +0900

    singleQuote

--- client/src/components/Board/index.js
+++ client/src/components/Board/index.js
@@ -1,4 +1,4 @@
-import "./style.scss";
+import './style.scss';
 
 function Board({
   title,
@@ -10,19 +10,19 @@ function Board({
   setBoardData,
 }) {
   return (
-    <div className="board" onClick={setBoardData}>
-      <div className="picture">
+    <div className='board' onClick={setBoardData}>
+      <div className='picture'>
         <img alt={title} src={imageLink} />
       </div>
-      <div className="contents">
-        <div className="title">{title}</div>
-        <div className="category-time">
-          <div className="category">{category}</div>
-          <div className="time">{time}</div>
+      <div className='contents'>
+        <div className='title'>{title}</div>
+        <div className='category-time'>
+          <div className='category'>{category}</div>
+          <div className='time'>{time}</div>
         </div>
-        <div className="money-user">
-          <div className="money">{price}</div>
-          <div className="user">{user}</div>
+        <div className='money-user'>
+          <div className='money'>{price}</div>
+          <div className='user'>{user}</div>
         </div>
       </div>
     </div>
