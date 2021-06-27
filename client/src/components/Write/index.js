cd7c389bd5183db1250d8b9ade84f951c3ac7e86
pkiop <pkiop@naver.com>
2021-06-27 22:07:56 +0900

    문자열 쌍따옴표로 감싸던 것 수정

--- client/src/components/Write/index.js
+++ client/src/components/Write/index.js
@@ -1,17 +1,17 @@
-import axios from "axios";
-import React, { useState } from "react";
-import Input from "./Input";
-import "./style.scss";
+import axios from 'axios';
+import React, { useState } from 'react';
+import Input from './Input';
+import './style.scss';
 
 function Write({ setBoardData, boardData, setVisible, fetchData }) {
-  const [title, setTitle] = useState(boardData?.title || "");
-  const [imageLink, setImageLink] = useState(boardData?.imageLink || "");
-  const [category, setCategory] = useState(boardData?.category || "");
-  const [price, setPrice] = useState(boardData?.price || "");
-  const [contents, setContents] = useState(boardData?.contents || "");
+  const [title, setTitle] = useState(boardData?.title || '');
+  const [imageLink, setImageLink] = useState(boardData?.imageLink || '');
+  const [category, setCategory] = useState(boardData?.category || '');
+  const [price, setPrice] = useState(boardData?.price || '');
+  const [contents, setContents] = useState(boardData?.contents || '');
 
   const createBoardData = async () => {
-    await axios.post("http://localhost:4000/api/board", {
+    await axios.post('http://localhost:4000/api/board', {
       title,
       imageLink,
       category,
@@ -23,7 +23,7 @@ function Write({ setBoardData, boardData, setVisible, fetchData }) {
   };
 
   const updateBoardData = async () => {
-    await axios.put("http://localhost:4000/api/board", {
+    await axios.put('http://localhost:4000/api/board', {
       _id: boardData._id, // 어떤 걸 수정해야 될 지 알려주어야 함
       title,
       imageLink,
@@ -38,7 +38,7 @@ function Write({ setBoardData, boardData, setVisible, fetchData }) {
 
   const deleteBoardData = async () => {
     // 1. 삭제하기 api 호출
-    await axios.delete("http://localhost:4000/api/board", {
+    await axios.delete('http://localhost:4000/api/board', {
       _id: boardData._id,
     });
     // 2. Write 안보이게 하기
@@ -51,28 +51,28 @@ function Write({ setBoardData, boardData, setVisible, fetchData }) {
 
   if (boardData === null) {
     return (
-      <div className="write">
-        <div className="inputs-wrapper">
-          <Input title={"글 제목"} value={title} setValue={setTitle} />
+      <div className='write'>
+        <div className='inputs-wrapper'>
+          <Input title={'글 제목'} value={title} setValue={setTitle} />
           <Input
-            title={"사진 링크"}
+            title={'사진 링크'}
             value={imageLink}
             setValue={setImageLink}
           />
-          <Input title={"카테고리"} value={category} setValue={setCategory} />
+          <Input title={'카테고리'} value={category} setValue={setCategory} />
           <Input
-            title={"가격"}
+            title={'가격'}
             value={price}
             setValue={setPrice}
-            inputType={"number"}
+            inputType={'number'}
           />
-          <Input title={"글 내용"} value={contents} setValue={setContents} />
-          <div className="button-wrapper">
-            <button className="green" onClick={createBoardData}>
+          <Input title={'글 내용'} value={contents} setValue={setContents} />
+          <div className='button-wrapper'>
+            <button className='green' onClick={createBoardData}>
               작성하기
             </button>
             <button
-              className="red"
+              className='red'
               onClick={() => {
                 setVisible(false);
               }}
@@ -86,27 +86,27 @@ function Write({ setBoardData, boardData, setVisible, fetchData }) {
   } else {
     // 여기는 수정하기
     return (
-      <div className="write">
-        <div className="inputs-wrapper">
-          <Input title={"글 제목"} value={title} setValue={setTitle} />
+      <div className='write'>
+        <div className='inputs-wrapper'>
+          <Input title={'글 제목'} value={title} setValue={setTitle} />
           <Input
-            title={"사진 링크"}
+            title={'사진 링크'}
             value={imageLink}
             setValue={setImageLink}
           />
-          <Input title={"카테고리"} value={category} setValue={setCategory} />
+          <Input title={'카테고리'} value={category} setValue={setCategory} />
           <Input
-            title={"가격"}
+            title={'가격'}
             value={price}
             setValue={setPrice}
-            inputType={"number"}
+            inputType={'number'}
           />
-          <Input title={"글 내용"} value={contents} setValue={setContents} />
-          <div className="button-wrapper">
-            <button className="green" onClick={updateBoardData}>
+          <Input title={'글 내용'} value={contents} setValue={setContents} />
+          <div className='button-wrapper'>
+            <button className='green' onClick={updateBoardData}>
               수정하기
             </button>
-            <button className="red" onClick={deleteBoardData}>
+            <button className='red' onClick={deleteBoardData}>
               삭제하기
             </button>
           </div>
