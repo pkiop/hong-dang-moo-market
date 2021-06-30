9e09fad3cb2fbca7457d29e562ad555aa478f346
pkiop <pkiop@naver.com>
2021-06-30 20:10:36 +0900

    category: 카테고리 페이지 추가

--- client/src/pages/Category/index.js
+++ client/src/pages/Category/index.js
@@ -1,32 +1,40 @@
 import './style.scss';
 import CategoryList from '../../components/CategoryList';
 import CategoryInput from '../../components/CategoryInput';
+import useApiCall from '../../hooks/useApiCall';
+import { useState } from 'react';
 
 function CategoryPage() {
-  const categoryList = [
-    {
-      title: '가전',
-    },
-    {
-      title: '노트북',
-    },
-    {
-      title: '가구',
-    },
-    {
-      title: '가전',
-    },
-    {
-      title: '노트북',
-    },
-    {
-      title: '가구',
-    },
-  ];
+  const [loading, categoryData, error, fetchData] = useApiCall(
+    'http://localhost:4000/api/category'
+  );
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  if (!categoryData) {
+    // null일경우
+    return <></>;
+  }
+
+  if (loading) {
+    return <>로딩중</>;
+  }
+
+  if (error) {
+    return <>{error}</>;
+  }
+
   return (
     <div className='category-page'>
-      <CategoryList categoryList={categoryList} />
-      <CategoryInput />
+      <CategoryList
+        categoryList={categoryData}
+        categoryFetch={fetchData}
+        setSelectedCategory={setSelectedCategory}
+      />
+      <CategoryInput
+        categoryFetch={fetchData}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
     </div>
   );
 }
