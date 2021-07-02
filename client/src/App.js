b01749f4d336280e9b68d23d138e6c806bc89095
pkiop <pkiop@naver.com>
2021-07-03 00:32:27 +0900

    auth: 유저 정보를 앱 전역에서 사용할 수 있도록 useReduser, useContext 사용해서 관리

--- client/src/App.js
+++ client/src/App.js
@@ -7,25 +7,52 @@ import Footer from './components/Footer';
 import './styles/reset.scss';
 import './styles/global-style.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useReducer, createContext } from 'react';
+// 프로젝트 환경변수 설정을 위해 (.env 파일의 변수를 가지고옴)
+import dotenv from 'dotenv';
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle';
+dotenv.config();
+
+const userReducer = (state, action) => {
+  switch (action.type) {
+    case 'signin':
+      return {
+        username: action.payload.username,
+        location: action.payload.location,
+      };
+    case 'signout':
+      return { username: '', location: '' };
+    default:
+      throw new Error();
+  }
+};
+
+export const UserContext = createContext(null);
 
 function App() {
+  const [user, dispatch] = useReducer(userReducer, {
+    username: '',
+    location: '',
+  });
+
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path='/category'>
-          <Category />
-        </Route>
-        <Route exact path='/'>
-          <Main />
-        </Route>
-        <Route path='/auth'>
-          <Auth />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+    <UserContext.Provider value={{ user, dispatch }}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path='/category'>
+            <Category />
+          </Route>
+          <Route exact path='/'>
+            <Main />
+          </Route>
+          <Route path='/auth'>
+            <Auth />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </UserContext.Provider>
   );
 }
 
