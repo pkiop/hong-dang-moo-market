c1485b8a44d0265140ad0488e3c6ff21154492a7
pkiop <pkiop@naver.com>
2021-07-02 22:15:12 +0900

    Header: Add auth routing at header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,60 @@
 import './style.scss';
+import { useHistory } from 'react-router-dom';
 
 function Header() {
-  return <div className="header">홍당무마켓</div>;
+  const history = useHistory();
+
+  return (
+    <div className='header'>
+      <div>홍당무마켓</div>
+      <div class='dropdown'>
+        <button
+          class='btn btn-secondary dropdown-toggle'
+          type='button'
+          id='dropdownMenu2'
+          data-bs-toggle='dropdown'
+          aria-expanded='false'
+        >
+          메뉴
+        </button>
+        <ul class='dropdown-menu' aria-labelledby='dropdownMenu2'>
+          <li>
+            <button
+              class='dropdown-item'
+              type='button'
+              onClick={() => {
+                history.push('/auth/signin');
+              }}
+            >
+              로그인
+            </button>
+          </li>
+          <li>
+            <button
+              class='dropdown-item'
+              type='button'
+              onClick={() => {
+                history.push('/auth/signout');
+              }}
+            >
+              로그아웃
+            </button>
+          </li>
+          <li>
+            <button
+              class='dropdown-item'
+              type='button'
+              onClick={() => {
+                history.push('/auth/signup');
+              }}
+            >
+              회원가입
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
