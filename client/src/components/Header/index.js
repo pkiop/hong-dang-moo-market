97f47e555f319534318846dc2d1a109acad0a01d
pkiop <pkiop@naver.com>
2021-07-03 00:32:57 +0900

    Header: 헤더에 로그아웃 버튼, 로그인한 username 보이도록 수정

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,48 +1,65 @@
 import './style.scss';
+import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
+import { UserContext } from '../../App';
+import axios from 'axios';
 
 function Header() {
+  const { user, dispatch } = useContext(UserContext);
   const history = useHistory();
+
+  const signoutClickHandler = async () => {
+    try {
+      await axios.delete(`${process.env.REACT_APP_API_SERVER}/api/auth`);
+      dispatch({ type: 'signout' });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const username = user?.username !== '' ? user.username : '';
 
   return (
     <div className='header'>
       <div>홍당무마켓</div>
-      <div class='dropdown'>
+      <div className='dropdown'>
         <button
-          class='btn btn-secondary dropdown-toggle'
+          className='btn btn-secondary dropdown-toggle'
           type='button'
           id='dropdownMenu2'
           data-bs-toggle='dropdown'
           aria-expanded='false'
         >
-          메뉴
+          {username === '' ? '메뉴' : username}
         </button>
-        <ul class='dropdown-menu' aria-labelledby='dropdownMenu2'>
+        <ul className='dropdown-menu' aria-labelledby='dropdownMenu2'>
+          {username === '' && (
+            <li>
+              <button
+                className='dropdown-item'
+                type='button'
+                onClick={() => {
+                  history.push('/auth/signin');
+                }}
+              >
+                로그인
+              </button>
+            </li>
+          )}
+          {username !== '' && (
+            <li>
+              <button
+                className='dropdown-item'
+                type='button'
+                onClick={signoutClickHandler}
+              >
+                로그아웃
+              </button>
+            </li>
+          )}
           <li>
             <button
-              class='dropdown-item'
-              type='button'
-              onClick={() => {
-                history.push('/auth/signin');
-              }}
-            >
-              로그인
-            </button>
-          </li>
-          <li>
-            <button
-              class='dropdown-item'
-              type='button'
-              onClick={() => {
-                history.push('/auth/signout');
-              }}
-            >
-              로그아웃
-            </button>
-          </li>
-          <li>
-            <button
-              class='dropdown-item'
+              className='dropdown-item'
               type='button'
               onClick={() => {
                 history.push('/auth/signup');
