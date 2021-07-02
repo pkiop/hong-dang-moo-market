6ba520b3c43fb97da26c8196faef0c21d1abb71a
pkiop <pkiop@naver.com>
2021-07-02 22:14:47 +0900

    App: Add auth routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import Main from './pages/Main';
 import Category from './pages/Category';
+import Auth from './pages/Auth';
 // import Counter from './pages/Counter';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './styles/reset.scss';
 import './styles/global-style.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle';
 
 function App() {
   return (
@@ -15,8 +17,11 @@ function App() {
         <Route path='/category'>
           <Category />
         </Route>
-        <Route path='/'>
+        <Route exact path='/'>
           <Main />
+        </Route>
+        <Route path='/auth'>
+          <Auth />
         </Route>
       </Switch>
       <Footer />
