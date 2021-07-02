185e884ec7622d41da8e886f67c0b0d5db29dad3
pkiop <pkiop@naver.com>
2021-07-02 22:16:58 +0900

    Auth: Add auth component / sign-in-up routing

---
+++ client/src/pages/Auth/index.js
@@ -0,0 +1,18 @@
+import { Route } from 'react-router-dom';
+import Signinup from '../../components/Signinup';
+
+function Auth() {
+  return (
+    <>
+      <Route path='/auth/signin'>
+        <Signinup isSignin />
+      </Route>
+      <Route path='/auth/signout'>로그아웃</Route>
+      <Route path='/auth/signup'>
+        <Signinup isSignin={false} />
+      </Route>
+    </>
+  );
+}
+
+export default Auth;
